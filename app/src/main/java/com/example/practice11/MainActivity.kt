9cbd9f644ed655dfe5cded5a5e27587499fab016
Your Name <you@example.com>
2022-10-27 22:13:44 +0300

    practice11

--- app/src/main/java/com/example/practice11/MainActivity.kt
+++ app/src/main/java/com/example/practice11/MainActivity.kt
@@ -3,20 +3,20 @@ package com.example.practice11
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
+import android.widget.ImageButton
 import android.widget.ImageView
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
     var isChecked = false
 
     fun onClick(view : View){
         val photo : ImageView = findViewById<ImageView>(R.id.imageView2);
-        val image: Int = R.drawable.catsleeping;
-        val butt=findViewById<Button>(R.id.button)
         photo.setImageResource(R.drawable.catsleeping);
         if(isChecked){
             photo.setImageResource(R.drawable.catsleeping)
@@ -24,21 +24,20 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-   /* private ImageView mImageView;
-    mImageView = (ImageView) findViewById(R.id.imageView2);
+    var on: Boolean = true
 
-    fun onClick2(view: View) {
-        val butt=findViewById<Button>(R.id.button)
-
-        if (isChecked){
-            btn1.visibility = View.INVISIBLE
-            btn2.visibility = View.VISIBLE
-            isChecked = !isChecked
-        }
-        else
+    fun onClickImage(view: View){
+        val btnImage: ImageButton = findViewById(R.id.imageButton)
+        if (on)
         {
-            btn2.visibility = View.INVISIBLE
-            btn1.visibility = View.VISIBLE
-            isChecked = !isChecked
-        }*/
+            btnImage.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.btn_check_buttonless_off))
+            on=false
+        }
+        else {
+            btnImage.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.btn_check_buttonless_on))
+            on=true
+        }
+    }
+
+
 }
